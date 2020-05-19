617a835e6b10746bdd85d0d6f40a1fe3a66b562f
Slavisa Babic <babic.slavisa@gmail.com>
2020-05-19 14:28:27 +0200

    dialog work

---
+++ client/src/components/roses/Roses.js
@@ -0,0 +1,139 @@
+import React, {useState, useEffect} from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Form from './Form.js';
+import Table from  './Table';
+import Dialog from './Dialog';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    container: {
+      paddingTop: theme.spacing(2),
+      paddingBottom: theme.spacing(1),
+    },
+    paper: {
+      padding: theme.spacing(1),
+      display: 'flex',
+      overflow: 'auto',
+      flexDirection: 'column',
+    },
+    fixedHeight: {
+      height: 140,
+    },
+  }));
+
+export default function Buyers() {
+  const [trigger, setTrigger] = useState (true);
+  const [editData, setEditData] = useState (false);
+  const [data, setData] = useState ([]);
+  const [open, setOpen] = React.useState(false);
+  const [rowData, setRowData] = React.useState("");
+  
+  useEffect(()=>{
+    getBuyers();
+  },[]);
+
+  const handleTrigger = ()=>{
+    setTrigger(trigger => !trigger);
+  };
+  
+  const getBuyers = async () => {
+    try{
+      const response = await fetch("http://localhost:5000/buyers");
+      const jsonData =await response.json();
+
+      setData(jsonData);
+      console.log(jsonData);
+
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  // Table edit data
+const handleEditData = (data) => {
+  console.log(data);
+  setRowData(data);
+  setEditData(true);
+};
+
+// From form
+const editDataHendler = () => {
+  setEditData(false);
+};
+
+// Table open dialog
+const handleOpenDialog = (data) => {
+  console.log(data);
+  setOpen(true);
+  setRowData(data);
+};
+
+// Delete from dialog
+const handleCloseDialog = () => {
+  setOpen(false);
+  console.log(rowData);
+  onDeleteClick(rowData);
+};
+
+const onDeleteClick = async (rowData) => {
+  try {
+    console.log("podaci za brisanje")
+    const deleteBuyers = await fetch (`http://localhost:5000/buyers/${rowData.buyer_id}`, {
+      method: "DELETE"
+  });
+    getBuyers();
+    console.log(deleteBuyers);
+
+  } catch (err) {
+    console.error(err.message);
+  }
+
+};
+
+
+// Close dialog
+const closeOpen = () => {
+  setOpen(false);
+} ;
+
+  const classes = useStyles();
+
+  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+  return (
+    <div className={classes.root}>
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={12} lg={12}>
+              <Table 
+                editData={handleEditData} 
+                openDialog={handleOpenDialog}
+                getBuyers={getBuyers}
+                data={data}
+                rowData={rowData}
+                />  
+            </Grid>  
+            <Grid item xs={12} md={12} lg={12}>
+              <Paper className={fixedHeightPaper}>
+                <Form
+                  setTrigger = {handleTrigger}
+                  editData = {editData}
+                  rowData={rowData}
+                  editDataHendler = {editDataHendler}
+                  getBuyers = {getBuyers}
+                />
+              </Paper>
+            </Grid>
+          </Grid>
+          <Dialog open = {open} closeOpen={closeOpen} rowData={rowData} handleCloseDialog={handleCloseDialog}/>
+        </Container>
+
+    </div>
+  );
+}
---
+++ client/src/components/roses/Table.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react';
+import MaterialTable from 'material-table';
+
+import { forwardRef } from 'react';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
+
+export default function MaterialTableDemo(props) {
+  const [columns, setColumns] = React.useState([
+    
+      { title: 'Naziv', field: 'name'},
+      { title: 'Adresa', field: 'address' },
+      { title: 'Grad', field: 'city'},
+      { title: 'Telefon', field: 'phone' },
+      { title: 'Mejl', field: 'email' },
+    
+  ]);
+
+  return (
+    <div>
+      <MaterialTable
+        title="Ruže"
+        columns={columns}
+        data={props.data}
+        icons={tableIcons}
+        options={{
+          headerStyle: {
+            fontWeight: 800
+          },
+        }}
+        actions={[
+          {
+            icon: Edit,
+            tooltip: 'Koriguj Kupca',
+            onClick: (event, rowData) => {props.editData(rowData)}
+          },
+          {
+            icon: DeleteOutline,
+            tooltip: 'Izbriši kupca',
+            onClick: (event, rowData) => {props.openDialog(rowData)}
+          }
+        ]}
+        
+      />
+    </div>
+  );
+
+}
