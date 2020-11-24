f1045565d24e4aff6947df9799814cc75e56422c
Slavisa Babic <babic.slavisa@gmail.com>
2020-11-24 11:24:20 +0100

    docker-compose

--- server/db.js
+++ server/db.js
@@ -3,7 +3,8 @@ const Pool = require ("pg").Pool;
 const pool = new Pool({
     user: "postgres",
     password: "postgresslavisa8",
-    host: "localhost",
+    // host: "localhost",
+    host:"pgdb",
     database: "flora_uno",
     port: 5432
 });
